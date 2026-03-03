54debda7dff2c85a08789299d06d7d8467c59431
Adam <Adamkardouchi11@gmail.com>
2026-03-03 19:41:58 +0000

    fix expense_service test

--- core/expense_service.py
+++ core/expense_service.py
@@ -82,7 +82,7 @@ class ExpenseService:
         return sum(expense.amount for expense in self._repository.list_all())
 
     def total_by_month(self) -> dict[str, float]:
-        totals = defaultdict(float)
+        totals: dict[str, float] = defaultdict(float)
 
         for expense in self._repository.list_all():
             key = expense.expense_date.strftime("%Y-%m")
